8cb5cbbb6e2e835a93cc459e7ba9c49b80881b4f
fazaadiguno <faza.adiguno@gmail.com>
2018-12-10 13:07:39 +0700

    Add files via upload

---
+++ precache-manifest.5b7048d069be363162aee3b9ecea7c47.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "0e6ee6030c68fc7e7dc6",
+    "url": "/static/css/main.195a6a2a.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "379b20850a6dbd1648ea",
+    "url": "/static/js/1.379b2085.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e6ee6030c68fc7e7dc6",
+    "url": "/static/js/main.0e6ee603.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "379b20850a6dbd1648ea",
+    "url": "/static/css/1.b4c135d3.chunk.css"
+  },
+  {
+    "revision": "18d2fc591ce8ae56a1ee10c45fad586c",
+    "url": "/index.html"
+  }
+];
